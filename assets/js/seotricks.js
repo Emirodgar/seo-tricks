29e09c5fd7d445ac53c83521f6585d5cf004ef4c
Emilio <emirodgar@gmail.com>
2020-02-21 12:48:46 +0100

    Update

---
+++ assets/js/seotricks.js
@@ -0,0 +1,25 @@
+function getContent(tab){
+
+    var spreadsheetID = "1PrRuSt-2IkZplJ3mwllo7YA3J025H65G0X46KtDJ4Wo";
+    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/" + tab + "/public/values?alt=json";
+
+    $.getJSON(url, function(data) {
+
+        var entry = data.feed.entry;
+
+
+        $(entry).each(function(){
+
+            if(tab==1){
+                var add = "<pre><code>"+this.gsx$rule.$t+"</code></pre>";
+            }
+            else if(tab==3){
+                var add = "<p>Author: <b>"+this.gsx$author.$t+"</b></p><ul class='actions'><li><a target='_blank' href="+this.gsx$link.$t+" class='button small'>Link</a></li></ul><hr>";
+            }
+
+            $('.results').append('<h2>'+this.title.$t+'</h2><p>'+this.gsx$description.$t+'</p>'+ add);
+
+          });
+
+        });
+}
